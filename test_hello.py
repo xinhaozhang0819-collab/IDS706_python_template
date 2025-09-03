cdb8f110af0c4b03009a346499f5a2a07ee86f30
xinhaozhang0819-collab <xinhaozhang0819@gmail.com>
2025-09-02 22:02:29 -0400

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,10 +1,17 @@
 from hello import say_hello, add
 
 def test_say_hello():
+    """
+    To test that function say_hello returns the expected string
+    when we offer a specific name input.
+    """
     assert (
         say_hello("Annie")
         == "Hello, Annie, welcome to Data Engineering Systems (IDS 706)!"
     )
 
 def test_add():
+    """
+    To test that the sum of 2 numbers give a correct output.
+    """
     assert add(2, 3) == 5
